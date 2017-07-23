261f4513071a5f1d059e9f0b53b4d466992c2e5b
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-23 20:54:45 +0300

    Created PageController

--- myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
+++ myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
@@ -1,4 +1,14 @@
 package main.java.com.myshop.myonlineshop.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
 public class PageController {
+
+    @RequestMapping(value = {"/", "/home", "/index"})
+    public ModelAndView index() {
+        return new ModelAndView("page");
+    }
 }
