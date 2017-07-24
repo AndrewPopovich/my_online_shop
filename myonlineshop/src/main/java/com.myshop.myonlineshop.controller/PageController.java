8f478f303c42fbb9f74da31bdb23714c8ab2bf40
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-24 20:26:53 +0300

    Added at PageController method for request mapping "/test"

--- myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
+++ myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
@@ -2,6 +2,7 @@ package com.myshop.myonlineshop.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -12,6 +13,15 @@ public class PageController {
         ModelAndView mv = new ModelAndView("page");
 
         mv.addObject("greeting", "Welcome to Spring Web MVC!");
+
+        return mv;
+    }
+
+    @RequestMapping(value = "/test")
+    public ModelAndView test(@RequestParam(value = "greeting") String greeting) {
+        ModelAndView mv = new ModelAndView("page");
+
+        mv.addObject("greeting", greeting);
 
         return mv;
     }
