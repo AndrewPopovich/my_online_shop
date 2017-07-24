7a8caebfae9346712b3828b7c1f1968ad35a1ea0
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-24 20:37:44 +0300

    Remove test method

--- myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
+++ myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
@@ -1,6 +1,7 @@
 package com.myshop.myonlineshop.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
@@ -13,15 +14,6 @@ public class PageController {
         ModelAndView mv = new ModelAndView("page");
 
         mv.addObject("greeting", "Welcome to Spring Web MVC!");
-
-        return mv;
-    }
-
-    @RequestMapping(value = "/test")
-    public ModelAndView test(@RequestParam(value = "greeting") String greeting) {
-        ModelAndView mv = new ModelAndView("page");
-
-        mv.addObject("greeting", greeting);
 
         return mv;
     }
