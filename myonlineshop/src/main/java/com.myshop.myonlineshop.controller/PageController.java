27f9315a06c09042c24ee584be6fd839dca4bf3c
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-24 20:59:38 +0300

    Added jquery

--- myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
+++ myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
@@ -1,9 +1,7 @@
 package com.myshop.myonlineshop.controller;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
