b68479b54f51c0129e8181c08495e759590c2e41
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-25 21:52:01 +0300

    Decide bugs with dependency to backend and added scan context component our in our backend from frontend

--- myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
+++ myonlineshop/src/main/java/com.myshop.myonlineshop.controller/PageController.java
@@ -1,13 +1,10 @@
 package com.myshop.myonlineshop.controller;
 
 import com.myshop.shopbackend.dao.CategoryDAO;
-import com.myshop.shopbackend.dto.Category;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
-
-import java.util.Locale;
 
 @Controller
 public class PageController {
