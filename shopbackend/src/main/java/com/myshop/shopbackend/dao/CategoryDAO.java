e7672175c0adc56554900640a88ee3bb309605dc
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-25 17:18:56 +0300

    Created intf CategoryDAO and impl CategoryDAOImpl

---
+++ shopbackend/src/main/java/com/myshop/shopbackend/dao/CategoryDAO.java
@@ -0,0 +1,10 @@
+package com.myshop.shopbackend.dao;
+
+import com.myshop.shopbackend.dto.Category;
+
+import java.util.List;
+
+public interface CategoryDAO {
+
+    List<Category> list();
+}
---
+++ shopbackend/src/main/java/com/myshop/shopbackend/daoimpl/CategoryDAOImpl.java
@@ -0,0 +1,13 @@
+package com.myshop.shopbackend.daoimpl;
+
+import com.myshop.shopbackend.dao.CategoryDAO;
+import com.myshop.shopbackend.dto.Category;
+
+import java.util.List;
+
+public class CategoryDAOImpl implements CategoryDAO {
+    @Override
+    public List<Category> list() {
+        return null;
+    }
+}
