e90aff74e4cb10e9ab721eaa6e86a79d43681779
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-26 16:04:48 +0300

    Created bean DataSource and configured it

--- shopbackend/src/main/java/com/myshop/shopbackend/config/HibernateConfig.java
+++ shopbackend/src/main/java/com/myshop/shopbackend/config/HibernateConfig.java
@@ -1,8 +1,12 @@
 package com.myshop.shopbackend.config;
 
+import org.apache.commons.dbcp2.BasicDataSource;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
 
 @Configuration
 @ComponentScan(basePackages = {"com.myshop.shopbackend.dto"})
@@ -18,4 +22,16 @@ public class HibernateConfig {
     private static final String DATABASE_USERNAME = "sa";
 
     private static final String DATABASE_PASSWORD = "";
+
+    @Bean
+    private DataSource getDataSource() {
+        BasicDataSource dataSource = new BasicDataSource();
+
+        dataSource.setUrl(DATABASE_URL);
+        dataSource.setDriverClassName(DATABASE_DRIVER);
+        dataSource.setUsername(DATABASE_USERNAME);
+        dataSource.setPassword(DATABASE_PASSWORD);
+
+        return dataSource;
+    }
 }
