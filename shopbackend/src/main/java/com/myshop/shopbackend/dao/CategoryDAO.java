ee8ef1b3ccd97f6e79ce0b03f29cbe8a02efaa96
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-26 21:30:49 +0300

    Added at intf CategoryDAO method add(category) and implemented that at CategoryDAOImpl

--- shopbackend/src/main/java/com/myshop/shopbackend/dao/CategoryDAO.java
+++ shopbackend/src/main/java/com/myshop/shopbackend/dao/CategoryDAO.java
@@ -6,6 +6,8 @@ import java.util.List;
 
 public interface CategoryDAO {
 
+    boolean add(Category category);
+
     List<Category> list();
 
     Category get(int id);
--- shopbackend/src/main/java/com/myshop/shopbackend/daoimpl/CategoryDAOImpl.java
+++ shopbackend/src/main/java/com/myshop/shopbackend/daoimpl/CategoryDAOImpl.java
@@ -2,13 +2,19 @@ package com.myshop.shopbackend.daoimpl;
 
 import com.myshop.shopbackend.dao.CategoryDAO;
 import com.myshop.shopbackend.dto.Category;
+import org.hibernate.SessionFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Repository("categoryDAO")
 public class CategoryDAOImpl implements CategoryDAO {
+
+    @Autowired
+    private SessionFactory sessionFactory;
 
     private static List<Category> categories = new ArrayList<>();
 
@@ -39,6 +45,18 @@ public class CategoryDAOImpl implements CategoryDAO {
         category3.setImageURL("Cat_3.png");
 
         categories.add(category3);
+    }
+
+    @Override
+    @Transactional
+    public boolean add(Category category) {
+        try {
+            sessionFactory.getCurrentSession().persist(category);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
     }
 
     @Override
