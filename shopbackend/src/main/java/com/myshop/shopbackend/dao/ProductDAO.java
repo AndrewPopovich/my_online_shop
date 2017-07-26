2d6d9a4f0f7bfcea6aebb3a046f9b72e5cab1d4b
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-27 01:23:29 +0300

    Created intf ProductDAO

---
+++ shopbackend/src/main/java/com/myshop/shopbackend/dao/ProductDAO.java
@@ -0,0 +1,25 @@
+package com.myshop.shopbackend.dao;
+
+import com.myshop.shopbackend.dto.Product;
+
+import java.util.List;
+
+public interface ProductDAO {
+
+    boolean add(Product category);
+
+    boolean update(Product category);
+
+    boolean delete(Product category);
+
+    List<Product> list();
+
+    Product get(int id);
+
+
+    List<Product> listActiveProducts();
+
+    List<Product> listActiveProductsByCategory();
+
+    List<Product> getLatestActiveProducts();
+}
