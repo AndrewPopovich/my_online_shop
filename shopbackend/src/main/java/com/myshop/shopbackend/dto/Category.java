86280c83871f8608e3949e4cd56111d2678c488d
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-26 21:21:24 +0300

    Added in class Category annotations and override method toString

--- shopbackend/src/main/java/com/myshop/shopbackend/dto/Category.java
+++ shopbackend/src/main/java/com/myshop/shopbackend/dto/Category.java
@@ -1,15 +1,26 @@
 package com.myshop.shopbackend.dto;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Category {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
     private String name;
 
     private String description;
 
+    @Column(name = "image_url")
     private String imageURL;
 
+    @Column(name = "iis_active")
     private boolean active = true;
 
     public int getId() {
@@ -50,5 +61,16 @@ public class Category {
 
     public void setActive(boolean active) {
         this.active = active;
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", description='" + description + '\'' +
+                ", imageURL='" + imageURL + '\'' +
+                ", active=" + active +
+                '}';
     }
 }
