41d6fb3d9b5ae0a8a82abd25b86f5037058f891d
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-27 23:51:40 +0300

    Fixed bugs with buttons

--- myonlineshop/src/main/webapp/assets/js/myapp.js
+++ myonlineshop/src/main/webapp/assets/js/myapp.js
@@ -56,8 +56,8 @@ $(function () {
                 data: 'id',
                 mRender: function (data, type, row) {
                     var str = '';
-                    str += '<a href="' + window.contextRoot + '/show/' + data + '/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open></span>" </a>';
-                    str += '<a href="' + window.contextRoot + '/cart/add/' + data + '/product class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart></span></a>';
+                    str += '<a href="' + window.contextRoot + '/show/' + data + '/product" class="btn btn-primary"><span class="glyphicon glyphicon-eye-open"></span></a>';
+                    str += '<a href="' + window.contextRoot + '/cart/add/' + data + '/product" class="btn btn-success"><span class="glyphicon glyphicon-shopping-cart"></span></a>';
 
                     return str;
                 }
