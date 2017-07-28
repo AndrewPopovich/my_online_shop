51308830dff6cb5e5701fe5aad6245179852bf4d
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-28 18:10:50 +0300

    Added handle method for 404 error

--- myonlineshop/src/main/java/com/myshop/myonlineshop/exception/GlobalDefaultExceptionHandler.java
+++ myonlineshop/src/main/java/com/myshop/myonlineshop/exception/GlobalDefaultExceptionHandler.java
@@ -1,7 +1,21 @@
 package com.myshop.myonlineshop.exception;
 
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.NoHandlerFoundException;
 
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
+
+    @ExceptionHandler(NoHandlerFoundException.class)
+    public ModelAndView handlerNoHandlerFoundException() {
+        ModelAndView mv = new ModelAndView();
+
+        mv.addObject("errorTitle", "The page is not constructed!");
+        mv.addObject("errorDescription", "The page you are looking for is not available now!");
+        mv.addObject("title", "404 Error Page");
+
+        return mv;
+    }
 }
