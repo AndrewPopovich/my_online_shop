ebab34f719af794d2420846931a87a4ff3c12ec4
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-31 17:51:40 +0300

    Created intf UserDAO

---
+++ shopbackend/src/main/java/com/myshop/shopbackend/dao/UserDAO.java
@@ -0,0 +1,14 @@
+package com.myshop.shopbackend.dao;
+
+import com.myshop.shopbackend.dto.Address;
+import com.myshop.shopbackend.dto.Cart;
+import com.myshop.shopbackend.dto.User;
+
+public interface UserDAO {
+
+    boolean addUser(User user);
+
+    boolean addAddress(Address address);
+
+    boolean addCart(Cart cart);
+}
