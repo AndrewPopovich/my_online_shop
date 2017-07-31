eed9a4cff3b29b6f63b73a65861d409659d05b19
AndrewPopovich <andrew1994popovich@gmail.com>
2017-07-31 22:39:49 +0300

    Added method listShippingAddresses in UserDAO

--- shopbackend/src/main/java/com/myshop/shopbackend/dao/UserDAO.java
+++ shopbackend/src/main/java/com/myshop/shopbackend/dao/UserDAO.java
@@ -4,6 +4,8 @@ import com.myshop.shopbackend.dto.Address;
 import com.myshop.shopbackend.dto.Cart;
 import com.myshop.shopbackend.dto.User;
 
+import java.util.List;
+
 public interface UserDAO {
 
     boolean addUser(User user);
@@ -12,7 +14,9 @@ public interface UserDAO {
 
     boolean addAddress(Address address);
 
-    Address getBillingAddress();
+    Address getBillingAddress(User user);
+
+    List<Address> listShippingAddresses(User user);
 
     boolean updateCart(Cart cart);
 }
