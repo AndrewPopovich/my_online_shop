ca5a4313313971dade9fe2ecca962819f6b631e1
AndrewPopovich <andrew1994popovich@gmail.com>
2017-08-30 21:40:35 +0300

    Added jquery validation for login form

--- myonlineshop/src/main/webapp/assets/js/myapp.js
+++ myonlineshop/src/main/webapp/assets/js/myapp.js
@@ -253,4 +253,34 @@ $(function () {
             }
         });
     }
+
+    var $loginForm = $('#loginForm');
+
+        if ($loginForm.length) {
+            $loginForm.validate({
+                rules: {
+                    username: {
+                        required: true,
+                        email: true
+                    },
+                    password: {
+                        required: true
+                    }
+                },
+                messages: {
+                    username: {
+                        required: 'Please enter the email!',
+                        email: 'Please enter valid email address!'
+                    },
+                    password: {
+                        required: 'Please enter the password!'
+                    }
+                },
+                errorElement: 'em',
+                errorPlacement: function (error, element) {
+                    error.addClass('help-block');
+                    error.insertAfter(element);
+                }
+            });
+        }
 });
