ca987a96d56ca3e928a2fa7b0ef6e72e2a72c624
AndrewPopovich <andrew1994popovich@gmail.com>
2017-09-01 21:01:13 +0300

    Added cartLines in ModelAndView at CartController

--- myonlineshop/src/main/java/com.myshop.myonlineshop.controller/CartController.java
+++ myonlineshop/src/main/java/com.myshop.myonlineshop.controller/CartController.java
@@ -1,5 +1,7 @@
 package com.myshop.myonlineshop.controller;
 
+import com.myshop.myonlineshop.service.CartService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
@@ -8,13 +10,16 @@ import org.springframework.web.servlet.ModelAndView;
 @RequestMapping("/cart")
 public class CartController {
 
+    @Autowired
+    private CartService service;
+
     @RequestMapping("/show")
     public ModelAndView showCart() {
         ModelAndView mv = new ModelAndView("page");
 
         mv.addObject("title", "User Cart");
         mv.addObject("userClickShowCart", true);
-        mv.addObject("cartLines", null);
+        mv.addObject("cartLines", service.getCartLines());
 
         return mv;
     }
