55a98b731bc45a1798886d1fc1fe8844b98d61c7
AndrewPopovich <andrew1994popovich@gmail.com>
2017-09-01 17:01:05 +0300

    Created dao implementation class CartLineDAOImpl

---
+++ shopbackend/src/main/java/com/myshop/shopbackend/daoimpl/CartLineDAOImpl.java
@@ -0,0 +1,37 @@
+package com.myshop.shopbackend.daoimpl;
+
+import com.myshop.shopbackend.dao.CartLineDAO;
+import com.myshop.shopbackend.dto.CartLine;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Repository("cartLineDAO")
+@Transactional
+public class CartLineDAOImpl implements CartLineDAO {
+    @Override
+    public CartLine get(int id) {
+        return null;
+    }
+
+    @Override
+    public boolean add(CartLine cartLine) {
+        return false;
+    }
+
+    @Override
+    public boolean update(CartLine cartLine) {
+        return false;
+    }
+
+    @Override
+    public boolean delete(CartLine cartLine) {
+        return false;
+    }
+
+    @Override
+    public List<CartLine> list(int cartId) {
+        return null;
+    }
+}
