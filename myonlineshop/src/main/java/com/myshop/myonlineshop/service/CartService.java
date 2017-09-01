5ca108870767979b5753c3fe37963f5b13c5d382
AndrewPopovich <andrew1994popovich@gmail.com>
2017-09-01 20:59:33 +0300

    Created method getCartLines

--- myonlineshop/src/main/java/com/myshop/myonlineshop/service/CartService.java
+++ myonlineshop/src/main/java/com/myshop/myonlineshop/service/CartService.java
@@ -3,10 +3,12 @@ package com.myshop.myonlineshop.service;
 import com.myshop.myonlineshop.model.UserModel;
 import com.myshop.shopbackend.dao.CartLineDAO;
 import com.myshop.shopbackend.dto.Cart;
+import com.myshop.shopbackend.dto.CartLine;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.servlet.http.HttpSession;
+import java.util.List;
 
 @Service("cartService")
 public class CartService {
@@ -19,5 +21,11 @@ public class CartService {
 
     private Cart getCart() {
         return ((UserModel) session.getAttribute("userModel")).getCart();
+    }
+
+    public List<CartLine> getCartLines() {
+        Cart cart = getCart();
+
+        return cartLineDAO.list(cart.getId());
     }
 }
