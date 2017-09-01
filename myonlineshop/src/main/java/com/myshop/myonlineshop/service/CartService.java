7c5521b55bc5826b519ccc461eec46262b176f23
AndrewPopovich <andrew1994popovich@gmail.com>
2017-09-01 20:57:50 +0300

    Created method getCart, it return cart from userModel

--- myonlineshop/src/main/java/com/myshop/myonlineshop/service/CartService.java
+++ myonlineshop/src/main/java/com/myshop/myonlineshop/service/CartService.java
@@ -1,12 +1,23 @@
 package com.myshop.myonlineshop.service;
 
+import com.myshop.myonlineshop.model.UserModel;
 import com.myshop.shopbackend.dao.CartLineDAO;
+import com.myshop.shopbackend.dto.Cart;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import javax.servlet.http.HttpSession;
 
 @Service("cartService")
 public class CartService {
 
     @Autowired
     private CartLineDAO cartLineDAO;
+
+    @Autowired
+    private HttpSession session;
+
+    private Cart getCart() {
+        return ((UserModel) session.getAttribute("userModel")).getCart();
+    }
 }
