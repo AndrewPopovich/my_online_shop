595798f8a19c086e9b6a3549885956e5d515a71a
AndrewPopovich <andrew1994popovich@gmail.com>
2017-09-04 23:56:17 +0300

    Remove hard-code from GlobalDefaultExceptionHandler

--- myonlineshop/src/main/java/com/myshop/myonlineshop/exception/GlobalDefaultExceptionHandler.java
+++ myonlineshop/src/main/java/com/myshop/myonlineshop/exception/GlobalDefaultExceptionHandler.java
@@ -1,5 +1,6 @@
 package com.myshop.myonlineshop.exception;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.servlet.ModelAndView;
@@ -11,31 +12,55 @@ import java.io.StringWriter;
 @ControllerAdvice
 public class GlobalDefaultExceptionHandler {
 
+    @Value("${excep.message.notConstr}")
+    private String notConstr;
+
+    @Value("${excep.message.notAvailb}")
+    private String notAvailable;
+
+    @Value("${excep.title.404}")
+    private String error404;
+
+    @Value("${page.jsp.errorJsp}")
+    private String error;
+
+    @Value("${excep.title.prodNotAvailb}")
+    private String productNotAvailable;
+
+    @Value("${excep.message.prodNotAvailbDes}")
+    private String productNotAvailableDescription;
+
+    @Value("${excep.title.prodUnavailb}")
+    private String productUnavailable;
+
+    @Value("${excep.title.contAdmin}")
+    private String contactAdmin;
+
     @ExceptionHandler(NoHandlerFoundException.class)
     public ModelAndView handlerNoHandlerFoundException() {
-        ModelAndView mv = new ModelAndView("error");
+        ModelAndView mv = new ModelAndView(error);
 
-        mv.addObject("errorTitle", "The page is not constructed!");
-        mv.addObject("errorDescription", "The page you are looking for is not available now!");
-        mv.addObject("title", "404 Error Page!");
+        mv.addObject("errorTitle", notConstr);
+        mv.addObject("errorDescription", notAvailable);
+        mv.addObject("title", error404);
 
         return mv;
     }
 
     @ExceptionHandler(ProductNotFoundException.class)
     public ModelAndView handlerProductNotFoundException() {
-        ModelAndView mv = new ModelAndView("error");
+        ModelAndView mv = new ModelAndView(error);
 
-        mv.addObject("errorTitle", "The product available!");
-        mv.addObject("errorDescription", "The product you are looking for is not available now!");
-        mv.addObject("title", "Product unavailable!");
+        mv.addObject("errorTitle", productNotAvailable);
+        mv.addObject("errorDescription", productNotAvailableDescription);
+        mv.addObject("title", productUnavailable);
 
         return mv;
     }
 
     @ExceptionHandler(Exception.class)
     public ModelAndView handlerException(Exception ex) {
-        ModelAndView mv = new ModelAndView("error");
+        ModelAndView mv = new ModelAndView(error);
 
         // TODO remove in production
         StringWriter sw = new StringWriter();
@@ -43,9 +68,9 @@ public class GlobalDefaultExceptionHandler {
 
         ex.printStackTrace(pw);
 
-        mv.addObject("errorTitle", "Contact you administrator!");
+        mv.addObject("errorTitle", contactAdmin);
         mv.addObject("errorDescription", sw.toString());
-        mv.addObject("title", "Error!");
+        mv.addObject("title", error.toUpperCase() + "!");
 
         return mv;
     }
